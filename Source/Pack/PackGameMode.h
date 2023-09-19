5a4ff97edb82a0e9d333b606f0e05a09679051cb
lyf <1075834244@qq.com>
2023-09-19 21:07:40 +0800

    MyPackForUE

---
+++ Source/Pack/PackGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PackGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APackGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APackGameMode();
+};
+
+
+
